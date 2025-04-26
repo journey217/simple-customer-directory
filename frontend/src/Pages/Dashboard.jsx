977996f076cc662bd53f223660b6a89e760c2765
Journey <journeys@buffalo.edu>
2025-04-26 15:11:43 -0400

    feat: Simple frontend display for fetching customers.

---
+++ frontend/src/Pages/Dashboard.jsx
@@ -0,0 +1,53 @@
+import {useEffect, useState} from "react";
+
+const Dashboard = () => {
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+
+        if (!isLoaded) {
+            fetchUsers()
+        }
+
+    }, [isLoaded]);
+
+    const fetchUsers = async () => {
+        let url = `${import.meta.env.VITE_API_PATH}/customers`;
+        try {
+            const response = await fetch(url, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            let reply = await response.json();
+            if (reply.success) {
+                console.log(reply)
+                setUsers(reply.data);
+
+            } else {
+                console.log(reply.message)
+            }
+            setIsLoaded(true)
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+
+    return (
+        <div>
+            {isLoaded &&
+                <div>
+                    {users.map(user => (
+                        <div key={user.id}>
+                            <span>{user.name}</span>
+                        </div>
+                    ))}
+                </div>
+            }
+        </div>
+    )
+}
+
+export default Dashboard
